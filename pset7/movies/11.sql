61b0660f847c283e4185f20fe24b5f27698ec524
petsop1 <120568009+petsop1@users.noreply.github.com>
2026-06-11 14:42:20 +0200

    Delete pset7 directory

--- pset7/movies/11.sql
+++
@@ -1,9 +0,0 @@
--- In 11.sql, write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
---     Your query should output a table with a single column for the title of each movie.
---     You may assume that there is only one person in the database with the name Chadwick Boseman.
-
-
-SELECT title FROM movies WHERE id IN
-   (SELECT movie_id FROM stars WHERE person_id =
-      (SELECT id FROM people WHERE name = "Chadwick Boseman"))
-ORDER BY (SELECT rating FROM ratings WHERE ratings.movie_id = movies.id) DESC LIMIT 5;
--- pset7/practice/favorites/favorites.sql
+++
@@ -1,5 +0,0 @@
-UPDATE show SET title = "How I Met Your Mother" WHERE title LIKE "how i met your mother";
-
-UPDATE show SET title = "Broklyn Nine-Nine" WHERE title LIKE "%99%";
-
-SELECT * FROM shows ORDER BY title;
