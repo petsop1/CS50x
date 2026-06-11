da09d9c08ca350c83d620962b4d8b61dfe7b5db0
petsop1 <120568009+petsop1@users.noreply.github.com>
2026-06-11 12:51:32 +0200

    Add files via upload

---
+++ pset8/src8/jQuery/five.js
@@ -0,0 +1,19 @@
+function turnPurple() {
+    document.getElementById('colorDiv').style.backgroundColor = 'purple';
+}
+
+function turnGreen() {
+    document.getElementById('colorDiv').style.backgroundColor = 'green';
+}
+
+function turnOrange() {
+    document.getElementById("colorDiv").style.backgroundColor = "Orange";
+}
+
+function turnRed() {
+    document.getElementById("colorDiv").style.backgroundColor = "Red";
+}
+
+function turnBlue() {
+    document.getElementById("colorDiv").style.backgroundColor = "Blue";
+}
---
+++ pset8/src8/jQuery/jquerycolor.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.jQButton').click(function() {
+        $('#colorDiv').css('background-color', this.innerHTML.toLowerCase());
+    });
+});
+
+
+
